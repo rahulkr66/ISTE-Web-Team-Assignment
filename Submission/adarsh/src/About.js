2b97bc7b49d21e126737d63031f1239112209ed2
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-11-17 09:18:17 +0530

    Basic structure of webpage done

--- Submission/adarsh/src/About.js
+++ Submission/adarsh/src/About.js
@@ -1,1 +1,17 @@
 import React from "react";
+import "./About.css";
+
+class About extends React.Component {
+    render() {
+        return(
+            <div className="about">
+                <div className="heading">About Us</div>
+                <div className="text">
+                    ISTE NITK strives to supplement the campus’ academic environment by conducting a host of different technical events, workshops, and annual projects, aimed at making learning fun, insightful and impactful. With a host of various Special Interest Groups, or SIGs, our community specializes in domains ranging from various branches of engineering, to business, literature and media, ISTE NITK constantly strives to develop its’ members technical capabilities, and nurture a sense of creativity
+                </div>
+            </div>
+        );
+    }
+}
+
+export default About;
--- Submission/adarsh/src/Main.js
+++ Submission/adarsh/src/Main.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Main.css";
 
+import About from "./About.js";
 import Cards from "./Cards.js";
 
 class Main extends React.Component {
     render() {
         return(
             <div className="main">
+                <About />
                 <Cards />
             </div>
         );
